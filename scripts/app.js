ed77740def44370f58a4c3b0d0b58b9660612f31
Alejo <alejodm12345@gmail.com>
2025-11-12 09:58:03 -0300

    Permitir cambiar categoría al salir del sistema

--- scripts/app.js
+++ scripts/app.js
@@ -657,7 +657,6 @@ const ActiveEntryCard = ({
                     id={`category-${id}`}
                     value={iceCreamCategory}
                     onChange={(e) => onCategoryChange(id, e.target.value)}
-                    disabled={isReadyToSave}
                     className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md shadow-sm disabled:bg-gray-200"
                 >
                     {iceCreamCategories.map(option => (
